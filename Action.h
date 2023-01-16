d4f0f404fac8b9c097dcb7fd139e8c9a9c2451cb
angelicazonta <118358702+angelicazonta@users.noreply.github.com>
2023-01-16 10:51:53 +0100

    Update Action.h

--- Action.h
+++ Action.h
@@ -19,6 +19,7 @@ private:
 	
 	
 public:
+	Action(){}
 	Action(ActionType type) : type(type) {}
 	Action(Coordinates source, Coordinates target) : type(UNIT_ACTION), source(source), target(target) {}
 	
