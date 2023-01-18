228a4774d57c2370f024f21d362293726f1528e0
giuliacassanego <118358700+giuliacassanego@users.noreply.github.com>
2023-01-18 13:19:36 +0100

    Update GamePlay.cpp

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -24,14 +24,14 @@ void GamePlay::playGame()
 			cout << endl << p1.getName() <<"'s grid:"<< endl<< p1.getGrid(); 
 			ComputerPlayer p2("Player2");
 			
-            //save in log file the initial position coordinates
+            		//save in log file the initial position coordinates
 			logFile << "Posizioni player1\n" << p1.getInitPos() << "\n";
 			logFile << "Posizioni player2\n" << p2.getInitPos() << "\n";
 			
 			p1.setOpponent(&p2);
 			p2.setOpponent(&p1);
 			
-            //choose the beginner of the game
+            		//choose the beginner of the game
 			int first = start();
 			Player* active;
 			if(first == 1)
@@ -44,19 +44,29 @@ void GamePlay::playGame()
 			}
 
 			logFile << "The first action is of " << active->getName() <<  "\n";
-            int n = 0; //counter of the number of actions executed
+            		int n = 0; //counter of the number of actions executed
 			try{
 				while(n < 10)
 				{
 					cout << endl << "Game turn of " << active->getName() << endl;
 					Action a = active->nextAction();
+					if(a.getType() == SHOW)
+					{
+						actionCoords.push_back("XX XX");
+					}
+					else if(a.getType() == CLEAR)
+					{
+						actionCoords.push_back("AA AA");
+					}
+					else
+					{
+						actionCoords.push_back(Coordinates::createString(a.getSource(), a.getTarget()));
+					}
 					active->play(a);
-					
-					logFile << active->getActionCoords();
-				
+									
 					if(a.getType() != CLEAR && a.getType() != SHOW)
 					{
-                        //switch the active player
+                       				 //switch the active player
 						if(active == &p1)
 						{
 							active = &p2;
@@ -74,6 +84,7 @@ void GamePlay::playGame()
 			{
 				cout << e.what() << endl;
 			}
+			logFile << getActionCoords();
 			break;
 		}
 		case CC:
@@ -83,35 +94,33 @@ void GamePlay::playGame()
 			ComputerPlayer p2("Player2");
 			cout << p2.getName() <<"'s grid:"<< endl<< p2.getGrid(); 
 			
-            //save in log file the initial position coordinates
+            		//save in log file the initial position coordinates
 			logFile << "Posizioni player1\n" << p1.getInitPos() << "\n";
 			logFile << "Posizioni player2\n" << p2.getInitPos() << "\n";
 			
 			p1.setOpponent(&p2);
 			p2.setOpponent(&p1);
 			
-            //randomly choose the beginner
+            		//randomly choose the beginner
 			int first = start();
 			Player* active = (first == 1 ? &p1 : &p2);
 			
 			logFile << "The first action is of " << active->getName() <<  "\n";
   
-            int n = 0;//counter of the number of actions executed
+            		int n = 0;//counter of the number of actions executed
 			try{
 				while(n < 10)
 				{       
 					cout << "Game turn of " << active->getName() << endl;
 					Action a = active->nextAction();
 					active->play(a);
-					
-					logFile << active->getActionCoords();
-					
+										
 					cout << active->getName() << "'s grid: " << endl << active->getGrid();
                     
-                    //switch the active player
+                    			//switch the active player
 					active = (active == &p1 ? &p2 : &p1);
 
-                    //as said in the readme.txt file, we chose to comment the command 'AA AA', in order to make the game more clean
+                    			//as said in the readme.txt file, we chose to comment the command 'AA AA', in order to make the game more clean
 					//if(a.getType() != CLEAR)
 					//{
 						n++;
@@ -123,6 +132,7 @@ void GamePlay::playGame()
 			{
 				cout << e.what() << endl;
 			}
+			logFile << getActionCoords();
 			break;
 		}
 		default:
@@ -136,4 +146,14 @@ int GamePlay::start()
     srand(time(NULL));
     int n = rand()%2+1; 
     return n;
+}
+
+string GamePlay::getActionCoords()
+{
+	string s;
+	for(int i = 0; i < actionCoords.size(); i++)
+	{
+		s += actionCoords[i] + "\n";
+	}
+	return s;
 }
